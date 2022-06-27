4cbf13cea596a128fad6ce4b0c50dd5437560f99
Miikka Saarinen <miikka.saarinen@awacademy.training>
2022-06-27 13:43:32 +0300

    Kokeilu

--- ClassLibraryDates/Class1.cs
+++ ClassLibraryDates/Class1.cs
@@ -4,5 +4,15 @@ namespace ClassLibraryDates
 {
     public class Class1
     {
+        public static void FuturePast()
+        {
+            bool futureOrPast;
+
+            if (startLearningDate > DateTime.Now)
+                futureOrPast = true;
+            else
+                futureOrPast = false;
+        }
     }
 }
+
